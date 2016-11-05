f4b194e0bc7fb0ce0853c30442fd5952af611754
Konstantin Läufer <klaeufer@gmail.com>
2016-11-05 02:07:23 -0500

    added description to argparse
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- picplacr.py
+++ picplacr.py
@@ -12,13 +12,14 @@ import datetime
 import flickr_api
 from flickr_api import Walker
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-t", "--track", required=True,
-                    help="GPX track")
-parser.add_argument("-a", "--album", required=True,
-                    help="Flickr album")
-parser.add_argument("-o", "--offset", required=True, type=int,
-                    help="hours to add to local timestamp on photos to get UTC")
+parser = argparse.ArgumentParser(
+    description='geotag pictures in a Flickr album based on a GPX track')
+parser.add_argument('-t', '--track', required=True,
+                    help='GPX track')
+parser.add_argument('-a', '--album', required=True,
+                    help='Flickr album')
+parser.add_argument('-o', '--offset', required=True, type=int,
+                    help='hours to add to local timestamp on photos to get UTC')
 args = parser.parse_args()
 
 TRACK = args.track
@@ -26,12 +27,12 @@ ALBUM = args.album
 OFFSET = args.offset
 AUTH_FILE = os.environ.get('HOME') + '/.flickrauth'
 
-print("Processing GPS track {}".format(TRACK))
+print('Processing GPS track {}'.format(TRACK))
 f = open(TRACK, 'r')
 gpx = gpxpy.parse(f)
 start_time = gpx.tracks[0].segments[0].points[0].time
 end_time = gpx.tracks[-1].segments[-1].points[-1].time
-print("This track starts at {} and ends at {}".format(start_time, end_time))
+print('This track starts at {} and ends at {}'.format(start_time, end_time))
 
 def all_points(gpx):
     for track in gpx.tracks:
@@ -42,7 +43,7 @@ def all_points(gpx):
 def point_at(ps, t):
     return next(p for p in ps if p.time > t)
                 
-print("Processing Flickr album {}".format(ALBUM))
+print('Processing Flickr album {}'.format(ALBUM))
 flickr_api.set_auth_handler(AUTH_FILE)
 user = flickr_api.test.login()
 photosets = user.getPhotosets()
@@ -54,6 +55,6 @@ for pic in w:
     if t > end_time:
         break
     if t < start_time:
-        print("Photo {} skipped".format(pic))
+        print('Photo {} skipped'.format(pic))
         continue
-    print("Photo {} was taken at {}".format(pic, point_at(all_points(gpx), t)))
+    print('Photo {} was taken at {}'.format(pic, point_at(all_points(gpx), t)))
